e51587e23e08d9987aec07d8849a119833bda522
konrad <knimixyt@gmail.com>
2021-07-12 19:31:39 +0200

    - linux support fixes

---
+++ Build/Release/Linux/bin/Vertex.glsl
@@ -0,0 +1,19 @@
+#version 330 core
+layout (location = 0) in vec2 Position;
+layout (location = 1) in vec2 TextureCoord;
+layout (location = 2) in float TextureIndex;
+layout (location = 3) in float QuadID;
+
+out vec2 F_TextureCoord;
+out float F_TextureIndex;
+out float F_QuadID;
+
+uniform mat4 V_Ortho;
+
+void main() {
+    F_TextureCoord = TextureCoord;
+    F_TextureIndex = TextureIndex;
+    F_QuadID = QuadID;
+
+    gl_Position = V_Ortho * vec4(Position, 0.0f, 1.0f);
+}
